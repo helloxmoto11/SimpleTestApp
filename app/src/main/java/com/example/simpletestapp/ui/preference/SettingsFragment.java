0308b0aea8a69992cec2582bc43b6ad88ad990a9
MannyDesktop <rhcplive11@gmail.com>
2021-06-20 10:50:00 -0500

    added periodic work request to SettingsFragment

--- app/src/main/java/com/example/simpletestapp/ui/preference/SettingsFragment.java
+++ app/src/main/java/com/example/simpletestapp/ui/preference/SettingsFragment.java
@@ -4,15 +4,20 @@ import android.os.Bundle;
 
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.SwitchPreference;
 import androidx.work.OneTimeWorkRequest;
+import androidx.work.PeriodicWorkRequest;
 import androidx.work.WorkManager;
 import androidx.work.WorkRequest;
 
 import com.example.simpletestapp.R;
 import com.example.simpletestapp.work.NotificationWorker;
 
+import java.util.concurrent.TimeUnit;
+
 public class SettingsFragment extends PreferenceFragmentCompat {
     public static final String WORK_TAG = "notification-work";
+    private WorkManager workManager;
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
@@ -21,14 +26,29 @@ public class SettingsFragment extends PreferenceFragmentCompat {
 
     @Override
     public boolean onPreferenceTreeClick(Preference preference) {
+        workManager = WorkManager.getInstance(requireContext());
         if (preference.getKey().equals(getString(R.string.notify))) {
             WorkRequest request = new OneTimeWorkRequest.Builder(NotificationWorker.class)
                     .addTag(WORK_TAG)
                     .build();
 
-            WorkManager.getInstance(requireContext()).enqueue(request);
+            workManager.enqueue(request);
+        }
 
+        if (preference.getKey().equals(getString(R.string.set_daily_notification))) {
+            SwitchPreference preference1 = (SwitchPreference) preference;
+            boolean checked = preference1.isChecked();
+            if (checked) {
+                WorkRequest periodicRequest =
+                        new PeriodicWorkRequest.Builder(
+                                NotificationWorker.class,
+                                15,
+                                TimeUnit.MINUTES)
+                                .addTag(WORK_TAG)
+                                .build();
 
+                workManager.enqueue(periodicRequest);
+            } else workManager.cancelAllWorkByTag(WORK_TAG);
         }
         return super.onPreferenceTreeClick(preference);
     }
