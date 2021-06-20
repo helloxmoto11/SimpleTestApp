716e71af5a885287af16bb91235686f399dd9b80
MannyDesktop <rhcplive11@gmail.com>
2021-06-20 09:05:26 -0500

    Added Dependencies and App implementation

---
+++ app/src/main/java/com/example/simpletestapp/SimpleApp.java
@@ -0,0 +1,12 @@
+package com.example.simpletestapp;
+
+import android.app.Application;
+
+public class SimpleApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
--- app/src/main/java/com/example/simpletestapp/ui/MainActivity.java
+++ app/src/main/java/com/example/simpletestapp/ui/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.simpletestapp;
+package com.example.simpletestapp.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.simpletestapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
